e3821c1de7d20bca136e36c32adcf637bbbfdb5a
Benjamin Kampmann <ben@parity.io>
2020-09-15 13:00:07 +0200

    more server niceties

--- bin/utils/remote-sign-server/src/main.rs
+++ bin/utils/remote-sign-server/src/main.rs
@@ -3,7 +3,7 @@ use structopt::StructOpt;
 use jsonrpc_http_server::jsonrpc_core::IoHandler;
 use jsonrpc_http_server::ServerBuilder;
 
-use tokio;
+use tokio::stream::StreamExt;
 
 use sc_cli::KeystoreParams;
 use sc_service::config::KeystoreConfig;
@@ -12,6 +12,9 @@ use sc_remote_signer::{
     RemoteSignerApi,
     server::GenericRemoteSignerServer
 };
+
+use tokio;
+use env_logger;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "substrate-remote-sign-server", about = "keystore Server for Substrate's JSON-RPC Remote Signing Protocol")]
@@ -23,8 +26,10 @@ struct Opt {
     #[structopt(long = "interface", short="-i", default_value="127.0.0.1")]
     interface: String,
 }
+
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let opt = Opt::from_args();
     let base_path = std::env::current_dir().unwrap();
     let keystore = match opt.keystore.keystore_config(&base_path) {
@@ -32,13 +37,13 @@ async fn main() {
             LocalKeystore::open(path, password).map_err(|e| format!("{:}", e))
         },
         Err(e) => Err(format!("{:}", e)),
-        Ok(KeystoreConfig::InMemory) => unreachable!()
+        Ok(_) => Err(format!("Only Local-Keystore Paramters supported")),
     }.unwrap();
 
     let server_addr = format!("{}:{}", opt.interface, opt.port).parse()
         .expect("Could not parse interface/port");
 
-    let (remote_server, receiver) = GenericRemoteSignerServer::proxy(keystore);
+    let (remote_server, mut receiver) = GenericRemoteSignerServer::proxy(keystore);
 
     let mut io = IoHandler::new();
     io.extend_with(RemoteSignerApi::to_delegate(remote_server));
@@ -48,7 +53,13 @@ async fn main() {
         .start_http(&server_addr)
         .unwrap();
 
-    tokio::spawn(receiver);
+    tokio::spawn(async move {
+        loop {
+            if receiver.next().await == None {
+                break
+            }
+        }
+    });
     let _ = tokio::task::spawn_blocking(move || {
         println!("Serving Remote Signer at {:}", server_addr);
         server.wait()
