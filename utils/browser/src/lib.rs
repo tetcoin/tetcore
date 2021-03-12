033c2071559bc47375b08391be39b2abe137231a
marlonhanks <marlon.hanks@kedia.co>
2021-03-12 01:51:19 -0500

    utilize tetsy-libp2p@0.36.0

--- utils/browser/src/lib.rs
+++ utils/browser/src/lib.rs
@@ -32,7 +32,7 @@ use futures::{
 };
 use std::pin::Pin;
 use sc_chain_spec::Extension;
-use libp2p_wasm_ext::{ExtTransport, ffi};
+use wasm_ext::{ExtTransport, ffi};
 
 pub use console_error_panic_hook::set_once as set_console_error_panic_hook;
 
