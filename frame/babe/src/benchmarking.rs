ebb18cc7ebe23f141972047653ba1cd88ca12235
marlonhanks <marlon.hanks@kedia.co>
2021-03-17 04:55:11 -0400

    inspect benchmark

--- frame/babe/src/benchmarking.rs
+++ frame/babe/src/benchmarking.rs
@@ -58,7 +58,7 @@ benchmarks! {
 			Decode::decode(&mut &EQUIVOCATION_PROOF_BLOB[..]).unwrap();
 
 		let equivocation_proof2 = equivocation_proof1.clone();
-		println!("{:?}", Decode::decode(&mut &EQUIVOCATION_PROOF_BLOB[..]).unwrap());
+		println!("{:?}", equivocation_proof1);
 	}: {
 		tp_consensus_babe::check_equivocation_proof::<Header>(equivocation_proof1);
 	} verify {
