4036ba826e664d02bac9667e6c0b8a4372c56447
marlonhanks <marlon.hanks@kedia.co>
2021-03-16 22:32:41 -0400

    use tc-chain-spec

--- client/chain-spec/src/lib.rs
+++ client/chain-spec/src/lib.rs
@@ -115,7 +115,7 @@ pub use chain_spec::{
 };
 pub use extension::{Group, Fork, Forks, Extension, GetExtension, get_extension};
 pub use tc_chain_spec_derive::{ChainSpecExtension, ChainSpecGroup};
-pub use chain_spec::{Properties, ChainType};
+pub use tp_chain_spec::{Properties, ChainType};
 
 use serde::{Serialize, de::DeserializeOwned};
 use tp_runtime::BuildStorage;
